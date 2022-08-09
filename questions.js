9dbc7133796355895fe146da9a9c6f3ec2cd6d00
Urjaswee Chatterjee <109411850+UrjasweeChatterjee@users.noreply.github.com>
2022-08-10 01:04:21 +0530

    Add files via upload
    It is a quiz app with a timer of 15seconds. The questions are teemed with web development questions.

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following CSS style property is used to specify an italic text?",
+    answer: "font-style",
+    options: [
+      "style",
+      "font-style",
+      "font",
+      "@font-face"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HTML is considered as ",
+    answer: "Markup language",
+    options: [
+      "programming language",
+      "oop language",
+      "high level language",
+      "markup language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+];
